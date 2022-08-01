e032f8111190c1fdfa6af587612c20bc4f596f60
dilshad.steyp <dilshad.steyp@gmail.com>
2022-08-01 11:49:06 +0530

    form

--- js/main.js
+++ js/main.js
@@ -145,5 +145,24 @@
 		skillsWayPoint();
 	});
 
+	$("#submit-form").submit((e)=>{
+		e.preventDefault()
+			$.ajax({
+				url:"https://formspree.io/f/mnqradrb",
+				data:$("#submit-form").serialize(),
+				method:"post",
+				success:function (response){
+					alert("Form submitted successfully")
+					window.location.reload()
+					//window.location.href="https://google.com"
+				},
+				error:function (err){
+					alert("Something Error")
+		
+				}
+			})
+	})
+		
+
 
 }());
